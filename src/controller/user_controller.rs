e29bb5db8a7f420a5a899cda579585bd126415ff
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2025-03-20 21:50:27 -0300

    Refactor user creation logic to use ValidateUserFields struct for validation and improve error handling

--- src/controller/user_controller.rs
+++ src/controller/user_controller.rs
@@ -1,6 +1,6 @@
 use actix_web::{post, web::{Data, Json}, HttpResponse};
 use entity::user::Model as User;
-use crate::{database::DbClient, model::user_model::PublicUser, validation::{structs::FieldError, user::{get_response_error, validate_user_fields}}};
+use crate::{database::DbClient, model::user_model::PublicUser, validation::user::{get_response_error, ValidateUserFields}};
 
 
 pub fn attach_service(app: &mut actix_web::web::ServiceConfig) {
@@ -8,34 +8,10 @@ pub fn attach_service(app: &mut actix_web::web::ServiceConfig) {
 }
 
 #[post("/user")]
-pub async fn create_user(db: Data<DbClient>, new_user: Json<User>) -> HttpResponse {
-    let mut errors = validate_user_fields(&new_user);
+pub async fn create_user(db_connection: Data<DbClient>, new_user: Json<User>) -> HttpResponse {
+    let validate_user = ValidateUserFields::new(db_connection.clone());
 
-    let email = match &new_user.email {
-        Some(email) => email.clone(),
-        None => {
-            errors.push(FieldError {
-                field_name: "Email".to_string(),
-                message: "Campo Email é obrigatório!".to_string(),
-            });
-
-            let response_error = get_response_error(errors);
-            return HttpResponse::BadRequest().json(response_error);
-        },
-    };
-
-    match db.user_dao.find_by_email(email.clone()).await {
-        Ok(exists) if exists => {
-            errors.push(FieldError {
-                field_name: "Email já existente".to_string(),
-                message: "Este Email já está cadastrado no sistema!".to_string(),
-            });
-        },
-        Err(err) => {
-            return HttpResponse::InternalServerError().json(format!("Erro ao verificar e-mail no banco de dados. Erro: {}", err));
-        },
-        _ => {}
-    }
+    let errors = validate_user.validate_user_fields(&new_user).await;
 
     if !errors.is_empty() {
         let response_error = get_response_error(errors);
@@ -45,12 +21,12 @@ pub async fn create_user(db: Data<DbClient>, new_user: Json<User>) -> HttpRespon
     let user_to_insert = User::new(
         new_user.name.clone(), 
         new_user.cpf.clone(),
-        Some(email), 
+        new_user.email.clone(), 
         Some("USUARIO".to_string()), 
         new_user.password.clone(),
     );
 
-    match db.user_dao.create(user_to_insert).await {
+    match db_connection.user_dao.create(user_to_insert).await {
         Ok(user) => HttpResponse::Created().json(PublicUser::from(user)),
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
     }
--- src/validation/user.rs
+++ src/validation/user.rs
@@ -1,39 +1,73 @@
-use actix_web::web::Json;
+use actix_web::web::{Data, Json};
 use entity::user::Model as User;
+use crate::database::DbClient;
 
 use super::structs::{FieldError, ResponseError};
 
-pub fn validate_user_fields(user: &Json<User>) -> Vec<FieldError> {
-    let mut errors: Vec<FieldError> = Vec::new();
+pub struct ValidateUserFields {
+    pub db_connection: Data<DbClient>
+}
 
-    if user.name.is_none() {
-        let error = FieldError {
-            field_name: "Campo Nome".to_string(),
-            message: "Campo Nome é obrigatório!".to_string()
-        };
-
-        errors.push(error);
+impl ValidateUserFields {
+    pub fn new(db_connection: Data<DbClient>) -> Self {
+        Self { db_connection }
     }
 
-    if user.cpf.is_none() {
-        let error = FieldError {
-            field_name: "Campo CPF".to_string(),
-            message: "Campo CPF é obrigatório!".to_string()
-        };
+    pub async fn validate_user_fields(&self, user: &Json<User>) -> Vec<FieldError> {
+        let mut errors: Vec<FieldError> = Vec::new();
 
-        errors.push(error);
+        if user.email.is_none() {
+            errors.push(FieldError {
+                field_name: "Email".to_string(),
+                message: "Campo Email é obrigatório!".to_string(),
+            });
+        }
+
+        match self.db_connection.user_dao.find_by_email(user.email.clone().unwrap()).await {
+            Ok(exists) if exists => {
+                errors.push(FieldError {
+                    field_name: "Email já existente".to_string(),
+                    message: "Este Email já está cadastrado no sistema!".to_string(),
+                });
+            },
+            Err(err) => {
+                errors.push(FieldError {
+                    field_name: "Houve um erro ao realizar a requisição".to_string(),
+                    message: format!("Houve um erro ao realizar a requisição: {err}").to_string(),
+                });
+            },
+            _ => {}
+        }
+    
+        if user.name.is_none() {
+            let error = FieldError {
+                field_name: "Campo Nome".to_string(),
+                message: "Campo Nome é obrigatório!".to_string()
+            };
+    
+            errors.push(error);
+        }
+    
+        if user.cpf.is_none() {
+            let error = FieldError {
+                field_name: "Campo CPF".to_string(),
+                message: "Campo CPF é obrigatório!".to_string()
+            };
+    
+            errors.push(error);
+        }
+    
+        if user.password.is_none() {
+            let error = FieldError {
+                field_name: "Campo Password".to_string(),
+                message: "Campo Password é obrigatório!".to_string()
+            };
+    
+            errors.push(error);
+        }
+    
+        return errors
     }
-
-    if user.password.is_none() {
-        let error = FieldError {
-            field_name: "Campo Password".to_string(),
-            message: "Campo Password é obrigatório!".to_string()
-        };
-
-        errors.push(error);
-    }
-
-    return errors
 }
 
 pub fn get_response_error(errors: Vec<FieldError>) -> ResponseError {
