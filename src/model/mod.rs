2c16cc711331cd7bec5889940df2313dc5bdc3ac
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2025-03-28 23:24:37 -0300

    Add ticket model with optional fields for client, title, description, status, payment method, discount, manpower, and associated products

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,2 +1,3 @@
 pub mod user_model;
 pub mod product_model;
+pub mod ticket_model;
---
+++ src/model/ticket_model.rs
@@ -0,0 +1,40 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(deny_unknown_fields)]
+pub struct OptionalTicket {
+    #[serde(rename = "client_id", skip_serializing_if = "Option::is_none")]
+    pub ticket_client_id: Option<Uuid>,
+
+    #[serde(rename = "title", skip_serializing_if = "Option::is_none")]
+    pub ticket_title: Option<String>,
+
+    #[serde(rename = "description", skip_serializing_if = "Option::is_none")]
+    pub ticket_description: Option<String>,
+
+    #[serde(rename = "status", skip_serializing_if = "Option::is_none")]
+    pub ticket_status: Option<String>,
+
+    #[serde(rename = "payment_method", skip_serializing_if = "Option::is_none")]
+    pub ticket_payment_method: Option<String>,
+
+    #[serde(rename = "discount", skip_serializing_if = "Option::is_none")]
+    pub ticket_discount: Option<f32>,
+
+    #[serde(rename = "manpower", skip_serializing_if = "Option::is_none")]
+    pub ticket_manpower: Option<f32>,
+
+    #[serde(rename = "products", skip_serializing_if = "Option::is_none")]
+    pub ticket_products: Option<Vec<OptionalProductTicket>>
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(deny_unknown_fields)]
+pub struct OptionalProductTicket {
+    #[serde(rename = "product_id", skip_serializing_if = "Option::is_none")]
+    pub ticket_product_id: Option<Uuid>,
+
+    #[serde(rename = "quantity", skip_serializing_if = "Option::is_none")]
+    pub quantity: Option<i32>
+}
