442f2c53d04b9254c9a9dc51ce9fcae24a1fef56
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2025-04-03 21:51:16 -0300

    Add update method to ProductDao for modifying product details

--- src/dao/product_dao.rs
+++ src/dao/product_dao.rs
@@ -41,5 +41,24 @@ impl ProductDao {
         let product = active_model.insert(&self.db_connection).await?;
         Ok(product)
     }
-    
+
+    pub async fn update(&self, product_id: Uuid, new_product: product::Model) -> Result<product::Model, Error> {
+        let product = product::Entity::find_by_id(product_id)
+            .one(&self.db_connection)
+            .await?;
+
+        match product {
+            Some(product) => {
+                let mut active_model: product::ActiveModel = product.into();
+
+                active_model.title = sea_orm::Set(new_product.title);
+                active_model.price = sea_orm::Set(new_product.price);
+                active_model.stock_quantity = sea_orm::Set(new_product.stock_quantity);
+
+                let updated_product = active_model.update(&self.db_connection).await?;
+                Ok(updated_product)
+            },
+            None => Err(Error::NotFound(format!("Product <id: {}>", product_id)))
+        }
+    }
 }
